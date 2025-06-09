4d80e5bed21f68f96cf67b474afa740616ad5159
andersonhavah <havahanderson@gmail.com>
2025-06-09 15:10:17 +0000

    assignment2.sql File

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -3,15 +3,14 @@ INSERT INTO account (account_firstname, account_lastname, account_email, account
 VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
 
 -- Query 2: Modify Tony Stark's account_type to Admin
--- Assuming account_email is a unique identifier for Tony Stark,
--- or you would have previously fetched his account_id.
+-- Assuming account_id is a unique identifier for Tony Stark
 UPDATE account
 SET account_type = 'Admin'
-WHERE account_email = 'tony@starkent.com';
+WHERE account_id = 1;
 
 -- Query 3: Delete Tony Stark record
 DELETE FROM account
-WHERE account_email = 'tony@starkent.com';
+WHERE account_id = 1;
 
 -- Query 4: Modify GM Hummer description
 UPDATE inventory
@@ -21,7 +20,7 @@ WHERE inv_make = 'GM' AND inv_model = 'Hummer';
 -- Query 5: Select Sport category vehicles
 SELECT i.inv_make, i.inv_model, c.classification_name
 FROM inventory AS i
-INNER JOIN classification AS c ON i.classification_id = c.classification_id
+JOIN classification AS c ON i.classification_id = c.classification_id
 WHERE c.classification_name = 'Sport';
 
 -- Query 6: Update inventory image and thumbnail paths
