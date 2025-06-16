c977418d92a564b6ac8dfd607d403f7b444643b4
andersonhavah <havahanderson@gmail.com>
2025-06-16 21:19:12 +0000

    Added code to get the vehicle data by inventory ID.

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -23,6 +23,21 @@ async function getInventoryByClassificationId(classification_id) {
     } catch (error) {
       console.error("getclassificationsbyid error " + error)
     }
+}
+
+/* ***************************
+ * Get vehicle data by inventory ID
+ * ************************** */
+async function getVehicleByInventoryId(inventory_id) {
+    try {
+      const sql = "SELECT * FROM public.inventory AS i JOIN public.classification AS c ON i.classification_id = c.classification_id WHERE i.inv_id = $1"
+      const data = await pool.query(sql, [inventory_id])
+      return data.rows[0] // Return the first (and only) row
+    } catch (error) {
+      console.error("getvehiclebyinventoryid error " + error)
+      return null // Return null if an error occurs or no vehicle is found
+    }
   }
 
-  module.exports = {getClassifications, getInventoryByClassificationId};
+
+  module.exports = {getClassifications, getInventoryByClassificationId, getVehicleByInventoryId};
