9c7e95a5ac9afceb8821232c9711ac2a8d510b9a
andersonhavah <havahanderson@gmail.com>
2025-06-17 21:02:21 +0000

    Deleted the index.js file in the route directory.

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-// Needed Resources 
-const express = require("express")
-const router = new express.Router() 
-const utilities = require("../utilities")
-const baseController = require("../controllers/baseController")
-
-// Route to trigger a 500 server error
-router.get("/trigger-error", utilities.handleErrors(baseController.triggerError));
