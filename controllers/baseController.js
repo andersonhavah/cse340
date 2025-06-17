31c553952b3fcec3aa348dca7c7a8ddfb37b95ad
andersonhavah <havahanderson@gmail.com>
2025-06-17 07:40:10 +0000

    Added more code for the intentional error

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -6,4 +6,13 @@ baseController.buildHome = async function (req, res) {
     res.render("index", {title: "Home", nav, description: "Welcome to the home page of CSE Motors."})
 }
 
+/* ***************************
+ * Trigger an intentional error
+ * ************************** */
+baseController.triggerError = async function(req, res, next) {
+    // This will cause a server error
+    const err = new Error("This is an intentional error for testing purposes.")
+    err.status = 500; // Set the status code to 500
+    throw err;
+}
 module.exports = baseController
