41fcea3f9fa3dca44520bd7cd8ccd264b99b09c7
andersonhavah <havahanderson@gmail.com>
2025-11-20 11:42:25 +0000

    Added the intentional error middleware and route.

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/index")
+const intentionalErrorRoute = require("./routes/intentionalErrorRoute");
+const catchIntentionalError = require("./middleware/catchIntentionalError");
 
 /* ***********************
  * View Engine and Templates
@@ -31,6 +33,12 @@ app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+// Intentional error route (for testing 500 handling)
+app.use("/error", intentionalErrorRoute);
+
+// Middleware to catch intentional errors from /error routes and render the error view
+app.use(catchIntentionalError);
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
