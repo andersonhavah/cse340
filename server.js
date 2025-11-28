7c1178c7155555de4a938ab3cb112a5a9c5c0743
andersonhavah <havahanderson@gmail.com>
2025-11-28 21:04:39 +0000

    Added middleware for the session.

--- server.js
+++ server.js
@@ -18,6 +18,19 @@ const catchIntentionalError = require("./middleware/catchIntentionalError");
 const session = require("express-session")
 const pool = require("./database/")
 
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
 
 /* ***********************
  * View Engine and Templates
