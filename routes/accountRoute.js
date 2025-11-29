bf380267f9aac6f6aeaffdc737af6a2a3ec947b6
andersonhavah <havahanderson@gmail.com>
2025-11-29 20:32:46 +0000

    Added the Route to build the Registration View.

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,8 +4,10 @@ const router = new express.Router();
 const utilities = require("../utilities/");
 const accountController = require("../controllers/accountController");
 
-// Deliver Login View
+// Route to build the Login View
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
+// Route to build the Registration View
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
 module.exports = router;
