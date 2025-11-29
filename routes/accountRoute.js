dff974af820d2db1ffef6968199b8e411d2bdbd1
andersonhavah <havahanderson@gmail.com>
2025-11-29 18:01:53 +0000

    Account Route with the Login View route

---
+++ routes/accountRoute.js
@@ -0,0 +1,11 @@
+// Needed Resources
+const express = require("express");
+const router = new express.Router();
+const utilities = require("../utilities/");
+const accountController = require("../controllers/accountController");
+
+// Deliver Login View
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+
+
+module.exports = router;
