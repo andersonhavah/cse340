cbad3ba7731bedd90e09b92262b781098a15e464
andersonhavah <havahanderson@gmail.com>
2025-11-29 21:46:43 +0000

    Added the Route to process the Registration View.

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -10,4 +10,7 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to build the Registration View
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
+// Route to process the Registration View
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
 module.exports = router;
