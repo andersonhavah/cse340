973490e9d8564150538a4794c205e72000dcfcb6
andersonhavah <havahanderson@gmail.com>
2025-11-30 10:08:47 +0000

    Imported body-parser into the scope of the app.

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const intentionalErrorRoute = require("./routes/intentionalErrorRoute");
 const catchIntentionalError = require("./middleware/catchIntentionalError");
 const session = require("express-session")
 const pool = require("./database/")
-const accountRoute =  require("./routes/accountRoute")
+const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
 
 /* ***********************
  * Middleware
@@ -39,6 +40,9 @@ app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View Engine and Templates
