a3596e10c6e673b4271e26d2dc5db36566470116
andersonhavah <havahanderson@gmail.com>
2025-12-02 14:10:32 +0000

    Imported bcryptjs to hash the user password and added the loginAccount function to process login request.

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,6 @@
 const utilities = require("../utilities");
 const accountModel = require("../models/account-model");
+const bcrypt = require("bcryptjs")
 
 /* ******************************
 *  Deliver login view
@@ -20,7 +21,7 @@ async function buildLogin(req, res, next) {
 async function buildRegister(req, res, next) {
     let nav = await utilities.getNav();
     res.render("account/register", {
-        title: "Register",
+        title: "Registration",
         nav,
         description: "CSE Motors Registration Page",
         errors: null,
@@ -33,12 +34,26 @@ async function buildRegister(req, res, next) {
 async function registerAccount(req, res) {
   let nav = await utilities.getNav()
   const { account_firstname, account_lastname, account_email, account_password } = req.body
-
+  
+    let hashedPassword
+    try {
+        // Regular password and cost (salt is generated automatically)
+        hashedPassword =  await bcrypt.hashSync(account_password, 10)
+    } catch (error) {
+        req.flash("notice", "Sorry there was an error processing the registration.")
+        res.status(500).render("account/register", {
+            title: "Registration",
+            nav,
+            description: "CSE MOTORS Registration Page",
+            errors: null,
+        })
+    }
+    
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   )
 
   if (regResult) {
@@ -63,4 +78,48 @@ async function registerAccount(req, res) {
   }
 }
 
-module.exports = { buildLogin, buildRegister, registerAccount };
+/* ****************************************
+ *  Process login request
+ * ************************************ */
+async function loginAccount(req, res) {
+  let nav = await utilities.getNav()
+  const { account_email, account_password } = req.body
+  const accountData = await accountModel.getAccountByEmail(account_email)
+  if (!accountData) {
+    req.flash("notice", "Please check your credentials and try again.")
+    res.status(400).render("account/login", {
+      title: "Login",
+      nav,
+      description: "CSE Motors Login Page",
+      errors: null,
+      account_email,
+    })
+    return
+  }
+  try {
+    if (await bcrypt.compare(account_password, accountData.account_password)) {
+      delete accountData.account_password
+      const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 1000 })
+      if(process.env.NODE_ENV === 'development') {
+        res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 })
+      } else {
+        res.cookie("jwt", accessToken, { httpOnly: true, secure: true, maxAge: 3600 * 1000 })
+      }
+      return res.redirect("/account/")
+    }
+    else {
+      req.flash("message notice", "Please check your credentials and try again.")
+      res.status(400).render("account/login", {
+        title: "Login",
+        nav,
+        description: "CSE Motors Login Page",
+        errors: null,
+        account_email,
+      })
+    }
+  } catch (error) {
+    throw new Error('Access Forbidden')
+  }
+}
+
+module.exports = { buildLogin, buildRegister, registerAccount, loginAccount };
