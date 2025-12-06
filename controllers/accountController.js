fee622ca4524dd9f4130628f19a095b9155767fe
andersonhavah <havahanderson@gmail.com>
2025-12-06 11:17:28 +0000

    Comment a piece of code.

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -96,30 +96,30 @@ async function loginAccount(req, res) {
     })
     return
   }
-  try {
-    if (await bcrypt.compare(account_password, accountData.account_password)) {
-      delete accountData.account_password
-      const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 1000 })
-      if(process.env.NODE_ENV === 'development') {
-        res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 })
-      } else {
-        res.cookie("jwt", accessToken, { httpOnly: true, secure: true, maxAge: 3600 * 1000 })
-      }
-      return res.redirect("/account/")
-    }
-    else {
-      req.flash("message notice", "Please check your credentials and try again.")
-      res.status(400).render("account/login", {
-        title: "Login",
-        nav,
-        description: "CSE Motors Login Page",
-        errors: null,
-        account_email,
-      })
-    }
-  } catch (error) {
-    throw new Error('Access Forbidden')
-  }
+//   try {
+//     if (await bcrypt.compare(account_password, accountData.account_password)) {
+//       delete accountData.account_password
+//       const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 1000 })
+//       if(process.env.NODE_ENV === 'development') {
+//         res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 })
+//       } else {
+//         res.cookie("jwt", accessToken, { httpOnly: true, secure: true, maxAge: 3600 * 1000 })
+//       }
+//       return res.redirect("/account/")
+//     }
+//     else {
+//       req.flash("message notice", "Please check your credentials and try again.")
+//       res.status(400).render("account/login", {
+//         title: "Login",
+//         nav,
+//         description: "CSE Motors Login Page",
+//         errors: null,
+//         account_email,
+//       })
+//     }
+//   } catch (error) {
+//     throw new Error('Access Forbidden')
+//   }
 }
 
 module.exports = { buildLogin, buildRegister, registerAccount, loginAccount };
