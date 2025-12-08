9832fb9c52841c796d4c7a38740a1162a0a36a7c
andersonhavah <havahanderson@gmail.com>
2025-12-08 15:26:57 +0000

    Added buildAccountManagement to build the account management view.

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -98,30 +98,43 @@ async function accountLogin(req, res) {
     })
     return
   }
-//   try {
-//     if (await bcrypt.compare(account_password, accountData.account_password)) {
-//       delete accountData.account_password
-//       const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 1000 })
-//       if(process.env.NODE_ENV === 'development') {
-//         res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 })
-//       } else {
-//         res.cookie("jwt", accessToken, { httpOnly: true, secure: true, maxAge: 3600 * 1000 })
-//       }
-//       return res.redirect("/account/")
-//     }
-//     else {
-//       req.flash("message notice", "Please check your credentials and try again.")
-//       res.status(400).render("account/login", {
-//         title: "Login",
-//         nav,
-//         description: "CSE Motors Login Page",
-//         errors: null,
-//         account_email,
-//       })
-//     }
-//   } catch (error) {
-//     throw new Error('Access Forbidden')
-//   }
+  try {
+    if (await bcrypt.compare(account_password, accountData.account_password)) {
+      delete accountData.account_password
+      const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 1000 })
+      if(process.env.NODE_ENV === 'development') {
+        res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 })
+      } else {
+        res.cookie("jwt", accessToken, { httpOnly: true, secure: true, maxAge: 3600 * 1000 })
+      }
+      return res.redirect("/account/")
+    }
+    else {
+      req.flash("message notice", "Please check your credentials and try again.")
+      res.status(400).render("account/login", {
+        title: "Login",
+        nav,
+        description: "CSE Motors Login Page",
+        errors: null,
+        account_email,
+      })
+    }
+  } catch (error) {
+    throw new Error('Access Forbidden')
+  }
 }
 
-module.exports = { buildLogin, buildRegister, registerAccount, accountLogin };
+/* ****************************************
+* Build Account Management View
+* ************************************ */
+async function buildAccountManagement(req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("account/management", {
+    title: "Account Management",
+    nav,
+    description: "Account Management Page.",
+    errors: null,
+  })
+}
+
+module.exports = { buildLogin, buildRegister, registerAccount, accountLogin, buildAccountManagement };
